c458d8b3b550cb76853efad7f203515ae47ecdf8
01222089646 <tranngochieu1003@gmail.com>
2018-08-07 19:33:23 +0700

    aaa

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './Component/LoginForm'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <LoginForm></LoginForm>
+      </div>
+    );
+  }
+}
+
+export default App;
