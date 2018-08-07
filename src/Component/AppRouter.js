11cba760f0171a768b55486dd7d6f2b685fd30b5
01222089646 <tranngochieu1003@gmail.com>
2018-08-07 20:58:43 +0700

    aaa

---
+++ src/Component/AppRouter.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Loadable from 'react-loadable';
+
+const Loading = () => <div>Loading..</div>
