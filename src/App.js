7fa34b82a95072d2ab8b43ccae3187abab966be0
01222089646 <tranngochieu1003@gmail.com>
2018-08-07 19:35:09 +0700

    aaa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import LoginForm from './Component/LoginForm'
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
         <LoginForm></LoginForm>
       </div>
     );
