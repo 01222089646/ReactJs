962fb3ac0df7165e9c67ab78d62892e282d28c63
01222089646 <tranngochieu1003@gmail.com>
2018-08-07 21:23:32 +0700

    aaa

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginForm from './Component/LoginForm'
+import Header from './Component/Header'
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header></Header>
         <LoginForm></LoginForm>
       </div>
     );
