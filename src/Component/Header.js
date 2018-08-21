a9d1895af46b21067e80f57513a8b151fff44e5a
01222089646 <tranngochieu1003@gmail.com>
2018-08-21 21:15:39 +0700

    aaa

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -6,20 +6,17 @@ import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 
 class Header extends React.Component {
   render() {
-    const header = {
-      backgroundColor: 'blue'
-      
-  };
+    
   
   
 
 
     return ( 
 
-      <Navbar fluid={true} inverse  collapseOnSelect  style={header} >
+      <Navbar fluid={true} inverse  collapseOnSelect  >
         <Navbar.Header>
           <Navbar.Brand >
-            <a href="#brand">Hình nền</a>
+            <a href="#brand"><img width={30} height={30} alt="" src="https://s.newsweek.com/sites/www.newsweek.com/files/styles/full/public/2018/04/26/0426moon.jpg" /></a>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
@@ -34,8 +31,9 @@ class Header extends React.Component {
 
           </Nav>
           <Nav pullRight >
-            <NavDropdown eventKey={3} title="Profile" id="basic-nav-dropdown"  >
-              
+            
+            <NavDropdown  eventKey={3} title="Profile" id="basic-nav-dropdown"  >
+                    
               <MenuItem eventKey={3.1}>Logout</MenuItem>
               <MenuItem eventKey={3.2} href="/profile">Profile</MenuItem>
             </NavDropdown>
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Carousel} from 'react-bootstrap'
 
 
 class Home extends React.Component {
@@ -14,7 +14,33 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                Đây là trang web mua sắm 
+                <br/>
+
+               
+               
+               <Carousel>
+  <Carousel.Item >
+    <img width={400} height={400} alt="" src="http://sohanews.sohacdn.com/thumb_w/660/2017/photo-6-1487059676040-64-0-436-600-crop-1487059888747.jpg" />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img width={400} height={400} alt="" src="https://vtv1.mediacdn.vn/thumb_w/650/2018/4/4/anh-1-tao-1-15228175859241411126029.jpg" />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img width={400} height={400} alt="" src="http://product.hstatic.net/1000198833/product/tao-red-delicious.png" />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>;
             </div>
         );
     }
